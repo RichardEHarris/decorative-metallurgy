cf5080cf8b1b12df0cdf49ff97c32348cc5add51
Richard Harris <bananaxxbolt@gmail.com>
2022-06-30 23:06:26 +0930

    Added slabs
    Implemented slabs to the block constructor.

--- src/main/java/net/bananabolt/decometal/ExampleMod.java
+++ src/main/java/net/bananabolt/decometal/ExampleMod.java
@@ -14,7 +14,8 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("decometal");
+	public static final String namespace = "decometal";
+	public static final Logger LOGGER = LoggerFactory.getLogger(namespace);
 
 	/* Declare and initialize our custom block instance.
        We set our block material to `METAL`, which requires a pickaxe to efficiently break.
@@ -26,13 +27,35 @@ public class ExampleMod implements ModInitializer {
        You can find the stats of all vanilla blocks in the class `Blocks`, where you can also reference other blocks.
     */
 	//Blocks
-	public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(1.0f).requiresTool());
+	//public static final Block EXAMPLE_BLOCK = new Block(FabricBlockSettings.of(Material.METAL).strength(1.0f).requiresTool());
 	@Override
 	public void onInitialize() {
-		//Blocks
-		Registry.register(Registry.BLOCK, new Identifier("decometal", "example_block"), EXAMPLE_BLOCK);
-		Registry.register(Registry.ITEM, new Identifier("decometal", "example_block"), new BlockItem(EXAMPLE_BLOCK, new FabricItemSettings().group(ItemGroup.MISC)));
 
+		//Blocks
+		newBlockFactory("example", Material.METAL, 1.0f, true,true, true, false, false);
 		LOGGER.info("Decorative Metal Initialized.");
 	}
+
+	private void newBlockFactory(String block_name, Material material, Float block_strength, boolean requires_tool, boolean do_block, boolean do_slab, boolean do_stair, boolean do_wall) {
+		Block new_block  = requires_tool ? new Block(FabricBlockSettings.of(material).strength(block_strength).requiresTool()) : new Block(FabricBlockSettings.of(material).strength(block_strength));
+		if (do_block) {
+			Registry.register(Registry.BLOCK, new Identifier(namespace, block_name+"_block"), new_block);
+			Registry.register(Registry.ITEM, new Identifier(namespace, block_name+"_block"), new BlockItem(new_block, new FabricItemSettings().group(ItemGroup.MISC)));
+		}
+		if (do_slab) {
+			Block new_slab = requires_tool ? new SlabBlock(FabricBlockSettings.of(material).strength(block_strength).requiresTool()) : new SlabBlock(FabricBlockSettings.of(material).strength(block_strength));
+			Registry.register(Registry.BLOCK, new Identifier(namespace, block_name+"_slab"), new_slab);
+			Registry.register(Registry.ITEM, new Identifier(namespace, block_name+"_slab"), new BlockItem(new_slab, new FabricItemSettings().group(ItemGroup.MISC)));
+		}
+		if (do_stair) {
+			Block new_stair = requires_tool ? new StairsBlock(new_block.getDefaultState() ,FabricBlockSettings.of(material).strength(block_strength).requiresTool()) : new StairsBlock(new_block.getDefaultState() ,FabricBlockSettings.of(material).strength(block_strength));
+			Registry.register(Registry.BLOCK, new Identifier(namespace, block_name+"_stair"), new_stair);
+			Registry.register(Registry.ITEM, new Identifier(namespace, block_name+"_stair"), new BlockItem(new_stair, new FabricItemSettings().group(ItemGroup.MISC)));
+		}
+		/*if (do_wall) {
+			Registry.register(Registry.BLOCK, new Identifier(namespace, block_name+"_wall"), block);
+			Registry.register(Registry.ITEM, new Identifier(namespace, block_name+"_wall"), new BlockItem(block, new FabricItemSettings().group(ItemGroup.MISC)));
+		}*/
+	}
+
 }
